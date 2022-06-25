4fff4f69b0b800373b92b628fdf80a009d062022
Vova2999 <Vova2999@gmail.com>
2022-06-25 16:19:26 +0400

    Create test project

---
+++ SudokuSolution.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SudokuSolution.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SudokuSolution.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be650ac0-eef5-4c2c-bf59-f6470af3c389")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SudokuSolution.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace SudokuSolution.Test {
+	[TestFixture]
+	public class UnitTest1 {
+		[Test]
+		public void TestMethod1() {
+		}
+	}
+}
