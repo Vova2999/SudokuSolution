7f3620294e2c07ff3192ffe72ada13eebee8c3e7
Vova2999 <Vova2999@gmail.com>
2022-06-26 19:16:15 +0400

    Fix duplicate code

--- SudokuSolution.Logic/FieldService/FieldService.cs
+++ SudokuSolution.Logic/FieldService/FieldService.cs
@@ -18,49 +18,45 @@ namespace SudokuSolution.Logic.FieldService {
 		public bool IsFailed(Field field) {
 			for (var row = 0; row < field.MaxValue; row++)
 			for (var column = 0; column < field.MaxValue; column++) {
-				if (field.Cells[row, column].HasFinal)
-					continue;
-
-				if (Enumerable.Range(1, field.MaxValue).All(value => !field.Cells[row, column][value]))
+				if (IsEmptyCell(field.Cells[row, column], field.MaxValue))
 					return true;
 			}
 
 			for (var row = 0; row < field.MaxValue; row++) {
 				var rowCells = field.Cells.SelectRow(row).ToArray();
-				if (rowCells.Any(cell => !cell.HasFinal))
-					continue;
-
-				var rowValues = rowCells.Select(cell => cell.Final).OrderBy(value => value).ToArray();
-				for (var value = 1; value <= field.MaxValue; value++) {
-					if (rowValues[value - 1] != value)
-						return true;
-				}
+				if (IsFailedCellsOrder(rowCells, field.MaxValue))
+					return true;
 			}
 
 			for (var column = 0; column < field.MaxValue; column++) {
 				var columnCells = field.Cells.SelectColumn(column).ToArray();
-				if (columnCells.Any(cell => !cell.HasFinal))
-					continue;
-
-				var columnValues = columnCells.Select(cell => cell.Final).OrderBy(value => value).ToArray();
-				for (var value = 1; value <= field.MaxValue; value++) {
-					if (columnValues[value - 1] != value)
-						return true;
-				}
+				if (IsFailedCellsOrder(columnCells, field.MaxValue))
+					return true;
 			}
 
 			var squareSize = (int) Math.Sqrt(field.MaxValue);
 			for (var squareRow = 0; squareRow < squareSize; squareRow++)
 			for (var squareColumn = 0; squareColumn < squareSize; squareColumn++) {
 				var squareCells = field.Cells.SelectSquare(squareSize, squareRow, squareColumn).ToArray();
-				if (squareCells.Any(cell => !cell.HasFinal))
-					continue;
+				if (IsFailedCellsOrder(squareCells, field.MaxValue))
+					return true;
+			}
 
-				var squareValues = squareCells.Select(cell => cell.Final).OrderBy(value => value).ToArray();
-				for (var value = 1; value <= field.MaxValue; value++) {
-					if (squareValues[value - 1] != value)
-						return true;
-				}
+			return false;
+		}
+
+		private static bool IsEmptyCell(Cell cell, int maxValue) {
+			return !cell.HasFinal && Enumerable.Range(1, maxValue).All(value => !cell[value]);
+		}
+
+		private static bool IsFailedCellsOrder(Cell[] rowCells, int maxValue) {
+			if (rowCells.Any(cell => !cell.HasFinal))
+				return false;
+
+			var rowValues = rowCells.Select(cell => cell.Final).OrderBy(value => value).ToArray();
+			for (var value = 1; value <= maxValue; value++) {
+				if (rowValues[value - 1] != value)
+					return true;
 			}
 
 			return false;
