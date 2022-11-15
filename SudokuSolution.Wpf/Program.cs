72d8c66652d3bb913b85d751a962509554a7df7c
Vova2999 <Vova2999@gmail.com>
2022-11-16 02:59:00 +0400

    Add references

---
+++ SudokuSolution.Wpf/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SudokuSolution.Wpf {
+	public class Program {
+		[STAThread]
+		private static void Main(string[] args) {
+			Locator.Current.Locate<App>().Run();
+		}
+	}
+}
